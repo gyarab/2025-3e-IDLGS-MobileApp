b3a5ca17bb1dcea9ad648a88838922ea772be269
AY-GA <ali.yunussov.s@gyarab.cz>
2026-01-14 22:05:38 +0100

    Added & Sorted util functions

---
+++ app/src/main/java/cz/idlgs/mobile/utils/UiUtils.kt
@@ -0,0 +1,63 @@
+package cz.idlgs.mobile.utils
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
+
+object UiUtils {
+	@Composable
+	fun orientation() = LocalConfiguration.current.orientation
+
+	@Composable
+	fun SplitIfLandscape(
+		isLandscape: Boolean,
+		modifier: Modifier = Modifier,
+		header: @Composable () -> Unit,
+		form: @Composable () -> Unit
+	) {
+		val scrollState = rememberScrollState()
+		if (isLandscape) {
+			Row(
+				modifier = modifier
+					.fillMaxSize()
+					.padding(16.dp),
+				verticalAlignment = Alignment.CenterVertically
+			) {
+				Column(
+					modifier = Modifier.weight(1f),
+					horizontalAlignment = Alignment.CenterHorizontally
+				) { header() }
+
+				Spacer(Modifier.width(24.dp))
+
+				Column(
+					modifier = Modifier
+						.weight(1f)
+						.verticalScroll(scrollState),
+					horizontalAlignment = Alignment.CenterHorizontally
+				) {
+					form()
+				}
+			}
+		} else {
+			Column(
+				modifier = modifier
+					.fillMaxSize()
+					.padding(16.dp)
+					.verticalScroll(scrollState),
+				horizontalAlignment = Alignment.CenterHorizontally,
+				verticalArrangement = Arrangement.Center
+			) {
+				Spacer(Modifier.height(16.dp))
+				header()
+				Spacer(Modifier.height(32.dp))
+				form()
+			}
+		}
+	}
+}
--- app/src/main/java/cz/idlgs/mobile/utils/Utils.kt
+++ app/src/main/java/cz/idlgs/mobile/utils/Utils.kt
@@ -1,13 +1,23 @@
 package cz.idlgs.mobile.utils
 
 import android.content.Context
+import android.content.Intent
 import android.widget.Toast
+import androidx.core.net.toUri
 
 object Utils {
-	var currentToast: Toast? = null;
+	val urlRegex = Regex("[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)+/?")
+	var currentToast: Toast? = null
+
 	fun Context.showToast(msg: String, long: Boolean = false) {
 		currentToast?.cancel()
 		currentToast = Toast.makeText(this, msg, if (long) Toast.LENGTH_LONG else Toast.LENGTH_SHORT)
 		currentToast!!.show()
+	}
+
+	fun Context.openLinkInBrowser(url: String) {
+		val intent = Intent(Intent.ACTION_VIEW)
+			.apply { data = url.toUri() }
+		startActivity(intent)
 	}
 }
