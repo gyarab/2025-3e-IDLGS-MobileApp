2558df5fd3f7c12d1bea1154cefed46b5bf837c1
AY-GA <ali.yunussov.s@gyarab.cz>
2026-01-29 15:56:43 +0100

    Created navGraphs for Compose Destinations

---
+++ app/src/main/java/cz/idlgs/mobile/nav/OverviewNavGraph.kt
@@ -0,0 +1,7 @@
+package cz.idlgs.mobile.nav
+
+import com.ramcosta.composedestinations.annotation.NavGraph
+import com.ramcosta.composedestinations.annotation.RootGraph
+
+@NavGraph<RootGraph>(defaultTransitions = HorizontalSlideTransitions::class)
+annotation class OverviewNavGraph
